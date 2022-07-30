5a9b2aa5a9c82f1940778511e3a81fd04ca3d479
pawanbenjamin <pawanbenjamin@gmail.com>
2022-07-29 23:31:02 -0500

    config removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-transition-link`, // Needed for dynamic images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
